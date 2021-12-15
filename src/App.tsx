54d90b72206df423051a25bb7d935d165f97d049
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2021-12-15 22:29:26 +0100

    Added devDep: Tailwind 3
    Install using PostCSS
    
      yarn add -D tailwindcss postcss autoprefixer
      yarn tailwindcss init -p

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,165 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    <>
+      <div className="App">
+        <div className="bg-white md:py-8 px-4 lg:max-w-7xl lg:mx-auto lg:px-8">
+          <p className="text-4xl font-bold text-gray-800 mb-8">March 2021</p>
+          <div className="inline-flex flex-col space-y-1 items-start justify-start h-full w-full">
+            <div className="inline-flex space-x-28 items-start justify-start pr-24 h-full w-full">
+              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
+                M
+              </p>
+              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
+                T
+              </p>
+              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
+                W
+              </p>
+              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
+                T
+              </p>
+              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
+                F
+              </p>
+              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
+                S
+              </p>
+              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
+                S
+              </p>
+            </div>
+            <div className="flex flex-col items-start justify-start">
+              <div className="inline-flex items-center justify-start h-full w-full">
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">01</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">02</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">03</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">04</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">05</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">06</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">07</p>
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-start w-full h-full">
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">08</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">09</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">10</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">11</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">12</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">13</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">14</p>
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-start w-full h-full">
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">15</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">16</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">17</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">18</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">19</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">20</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">21</p>
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-start h-full w-full">
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">22</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">23</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">24</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">25</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">26</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">27</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">28</p>
+                </div>
+              </div>
+              <div className="inline-flex items-center justify-start w-full h-full">
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">29</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">30</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="text-sm font-medium text-gray-800">31</p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="opacity-50 text-sm font-medium text-gray-800">
+                    01
+                  </p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="opacity-50 text-sm font-medium text-gray-800">
+                    02
+                  </p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="opacity-50 text-sm font-medium text-gray-800">
+                    03
+                  </p>
+                </div>
+                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
+                  <p className="opacity-50 text-sm font-medium text-gray-800">
+                    04
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
