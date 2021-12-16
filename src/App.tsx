65678c3e2061dfc6327d62c8e766232d7b2d6a77
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2021-12-16 01:23:53 +0100

    Composing the Calendar Template

--- src/App.tsx
+++ src/App.tsx
@@ -1,158 +1,65 @@
 import "./App.css";
+import { Styles_Headers_Day } from "./styles/Headers/Day";
+import { Styles_Bodies_DayEnd } from "./styles/Bodies/Day";
+import { Styles_Bodies_Day } from "./styles/Bodies/Day";
+import { Styles_Headers_Month } from "./styles/Headers/Month";
 
 function App() {
+  const dayNames = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  const events = ["event1", "event2"];
+
+  const dayList = Array.from(Array(7).keys());
   return (
     <>
       <div className="App">
         <div className="bg-white md:py-8 px-4 lg:max-w-7xl lg:mx-auto lg:px-8">
-          <p className="text-4xl font-bold text-gray-800 mb-8">March 2021</p>
+          <Styles_Headers_Month>March 2021</Styles_Headers_Month>
           <div className="inline-flex flex-col space-y-1 items-start justify-start h-full w-full">
             <div className="inline-flex space-x-28 items-start justify-start pr-24 h-full w-full">
-              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
-                M
-              </p>
-              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
-                T
-              </p>
-              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
-                W
-              </p>
-              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
-                T
-              </p>
-              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
-                F
-              </p>
-              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
-                S
-              </p>
-              <p className="w-12 h-full text-sm font-medium text-gray-800 uppercase">
-                S
-              </p>
+              {dayNames.map((day) => (
+                <Styles_Headers_Day key={day}>{day}</Styles_Headers_Day>
+              ))}
             </div>
             <div className="flex flex-col items-start justify-start">
               <div className="inline-flex items-center justify-start h-full w-full">
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">01</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">02</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">03</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">04</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">05</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">06</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">07</p>
-                </div>
+                {dayList.map((day) => (
+                  <Styles_Bodies_Day key={day}>
+                    {events.map((e) => (
+                      <div className="bg-red-800 px-8 m-2" key={e}>
+                        {e}
+                      </div>
+                    ))}
+                  </Styles_Bodies_Day>
+                ))}
               </div>
               <div className="inline-flex items-center justify-start w-full h-full">
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">08</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">09</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">10</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">11</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">12</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">13</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">14</p>
-                </div>
+                {dayList.map((day) => (
+                  <Styles_Bodies_Day key={day}>{day}</Styles_Bodies_Day>
+                ))}
               </div>
               <div className="inline-flex items-center justify-start w-full h-full">
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">15</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">16</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">17</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">18</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">19</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">20</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">21</p>
-                </div>
+                {dayList.map((day) => (
+                  <Styles_Bodies_Day key={day}>{day}</Styles_Bodies_Day>
+                ))}
               </div>
               <div className="inline-flex items-center justify-start h-full w-full">
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">22</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">23</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">24</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">25</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">26</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">27</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">28</p>
-                </div>
+                {dayList.map((day) => (
+                  <Styles_Bodies_Day key={day}>{day}</Styles_Bodies_Day>
+                ))}
               </div>
               <div className="inline-flex items-center justify-start w-full h-full">
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">29</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">30</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="text-sm font-medium text-gray-800">31</p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="opacity-50 text-sm font-medium text-gray-800">
-                    01
-                  </p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="opacity-50 text-sm font-medium text-gray-800">
-                    02
-                  </p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="opacity-50 text-sm font-medium text-gray-800">
-                    03
-                  </p>
-                </div>
-                <div className="flex items-start justify-start w-40 h-full pl-2 pr-32 pt-2.5 pb-24 border border-gray-200">
-                  <p className="opacity-50 text-sm font-medium text-gray-800">
-                    04
-                  </p>
-                </div>
+                {dayList.map((day) => (
+                  <Styles_Bodies_DayEnd key={day}>{day}</Styles_Bodies_DayEnd>
+                ))}
               </div>
             </div>
           </div>
