16c97e4b166ba5e3d9d12ad4c71717bba3e67544
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-01-26 11:55:51 +0100

    Fix removing commited layouts

--- src/pages/Codelink.tsx
+++ src/pages/Codelink.tsx
@@ -1,44 +1,73 @@
 import { useState } from "react";
+import { Month } from "@components/Month/main";
+import { CreateEvent } from "@/components/Controller/main";
+import * as tw_Topnav from "@components/Topnav/tw";
 import * as tw_Layouts from "@/layouts/tw";
-import { Container, Logo, Title, Logout } from "@components/Topnav/tw";
-//import { CreateEvent } from "@components/Controller/main";
-//import { EventsCtx } from "@context/eventsarray";
-//import { BoardLayout } from "@layouts/Board";
-//import { ControllerLayout } from "@layouts/Controller";
-import { MainLayout } from "@/layouts/LayoutMain";
-import { HeaderLayout } from "@/layouts/LayoutHeader";
+import { EventsCtx } from "@context/eventsarray";
+import * as initEvent from "@/static/initEvents";
 
+interface EventProps {
+  date: string;
+}
+
+/*
+const ThrowEventsArray: React.FC<EventProps> = ({ date }): JSX.Element => {
+  const { events } = useContext(EventsCtx);
+
+  return (
+    <>
+      {events
+        .filter((event) => event.start.includes(date))
+        .map((event) => (
+          <Event {...e}></Event>
+        ))}
+    </>
+  );
+};
+*/
 export default function App(): JSX.Element {
-  const [displayController, setDisplayController] = useState(false);
+  const [toogleCreate, setToogleCreate] = useState(false);
 
   return (
     <>
       {/*App*/}
       <tw_Layouts.app>
         {/*header-layout*/}
-
-        <tw_Layouts.header
-          onClick={() => setDisplayController((prev) => !prev)}
-        >
+        <tw_Layouts.header onClick={() => setToogleCreate((prev) => !prev)}>
           {/*header*/}
-          <Container>
+          <tw_Topnav.container>
             {/*left-header*/}
-            <Logo>JH Diary</Logo>
-            {/*center-header*/} <Title>Friday, 21 January 2022</Title>
+            <tw_Topnav.logo>JH Diary</tw_Topnav.logo>
+            {/*center-header*/}{" "}
+            <tw_Topnav.title>Friday, 21 January 2022</tw_Topnav.title>
             {/*right-header*/}
-            <Logout
+            <tw_Topnav.logout
               title={"Cleans up your session token | Ctrl+Alt+q"}
               onClick={(evt) => {
                 evt.stopPropagation();
               }}
             >
               Logout
-            </Logout>
-          </Container>
+            </tw_Topnav.logout>
+          </tw_Topnav.container>
         </tw_Layouts.header>
 
         {/*main-layout: layout-grid*/}
-        <MainLayout display={displayController} />
+        <tw_Layouts.main $display={toogleCreate}>
+          {/*controller-layout*/}
+          <tw_Layouts.controller $display={toogleCreate}>
+            {/* this sticky could be removed */}
+            <CreateEvent />
+          </tw_Layouts.controller>
+
+          {/*calendar-layout*/}
+          {/*calendar*/}
+          <tw_Layouts.board>
+            <Month events={initEvent.month1} />
+            <Month events={initEvent.month2} />
+            <Month events={initEvent.month1} />
+          </tw_Layouts.board>
+        </tw_Layouts.main>
       </tw_Layouts.app>
     </>
   );
