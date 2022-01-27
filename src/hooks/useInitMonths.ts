11b71d7a0a814f10bfca9e7f3cee81863e9b68e3
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-01-27 13:09:07 +0100

    custom Hook: generate a list of months of determined length

---
+++ src/hooks/useInitMonths.ts
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { DateService } from "@/utils/Date";
+
+export function useInitMonths(length?: number) {
+  //Case for one month
+  if (typeof length === "undefined" || length <= 1) {
+    return useState([DateService.GetDateNextMonth()]);
+  }
+
+  //Case for many months, here length is > 1
+
+  //Load first element
+  const monthList = [DateService.GetDateNextMonth()];
+  for (let i = 1; i < length; i++) {
+    monthList.push(
+      DateService.GetDateNextMonth(
+        monthList[i - 1].year,
+        monthList[i - 1].month
+      )
+    );
+  }
+  return useState(monthList);
+}
--- src/layouts/Board.tsx
+++ src/layouts/Board.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
 import { TWboard } from "./tw";
 import { Month } from "@/components/Month/main";
 import { month1 } from "@/static/initEvents";
 import { EventsProvider } from "@/context/eventState";
-import { DateService } from "@/utils/Date";
+import { useInitMonths } from "@/hooks/useInitMonths";
 export const LayoutBoard = () => {
-  const month_entry_0 = DateService.GetDateNextMonth();
-  const month_entry_1 = DateService.GetDateNextMonth(
-    month_entry_0.year,
-    month_entry_0.month
-  );
-  const [monthKeys, setMonthKeys] = useState([month_entry_0, month_entry_1]);
+  const [monthKeys, setMonthKeys] = useInitMonths(4);
+  console.log(monthKeys);
   return (
     <TWboard>
       <EventsProvider.Provider value={month1}>
