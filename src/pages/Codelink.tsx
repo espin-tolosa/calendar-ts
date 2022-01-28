3f011378629b52e36f3a4480303a87b741eda324
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-01-28 14:23:52 +0100

    Force scale

--- src/pages/Codelink.tsx
+++ src/pages/Codelink.tsx
@@ -1,5 +1,7 @@
 import { LayoutMain } from "@/layouts/Main";
 
 export default function App(): JSX.Element {
+  const scale = "scale(1)";
+  document.body.style.transform = scale;
   return <LayoutMain />;
 }
