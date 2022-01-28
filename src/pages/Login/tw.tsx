bac57fc0453cb58edddb3d15c44ebd3046dc6ac7
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-01-28 14:00:54 +0100

    Adjust Form styles

--- src/pages/Login/tw.tsx
+++ src/pages/Login/tw.tsx
@@ -5,13 +5,13 @@ flex justify-center items-center h-full
 `;
 
 export const TWloginForm = tw.form`
-flex flex-col relative mx-0 my-auto max-w-md p-5 
+flex flex-col relative mt-10 my-auto max-w-md
 `;
 
 export const TWloginInput = tw.input`
-p-2 bg-yellow-50 text-orange-500 mb-4 rounded-md outline-none transition-shadow shadow-sm hover:shadow-md focus:shadow-md 
+py-2 bg-yellow-50 text-orange-500 mb-4 rounded-md outline-none transition-shadow shadow-sm hover:shadow-md focus:shadow-md 
 `;
 
 export const TWloginButton = tw.button`
-p-4 text-yellow-50 uppercase bg-orange-500 border-none rounded-md outline-none cursor-pointer mt-3 shadow-md transition-colors hover:bg-orange-600
+py-4 text-yellow-50 uppercase bg-orange-500 border-none rounded-md outline-none cursor-pointer mt-3 shadow-md transition-colors hover:bg-orange-600
 `;
