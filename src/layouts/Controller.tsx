d62cfaa0433ee23d54109f2a99f7b6cf9dfa519e
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-02-11 10:43:57 +0100

    Testing display controller bug on mobile

--- src/layouts/Controller.tsx
+++ src/layouts/Controller.tsx
@@ -4,6 +4,10 @@ import { CreateEvent } from "@/components/Controller/main";
 import { useLocalUserPreferencesContext } from "@/hooks/useLocalUserPreferences";
 export const LayoutController = () => {
   const { localState } = useLocalUserPreferencesContext();
+  console.log(
+    "Displaycontroller from localState",
+    localState.displayController
+  );
   return (
     <tw_Layouts.TWcontroller $display={localState.displayController}>
       <CreateEvent />
