d6be18066d81723c1dabf5ddfa5b95ee6bacf1dd
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-02-15 21:50:20 +0100

    Avoid auto scale on mobile devices

--- src/static/defaultUserState.ts
+++ src/static/defaultUserState.ts
@@ -1,5 +1,5 @@
 export const defaultUserState = {
-  displayController: true,
+  displayController: false,
   darkMode: false,
   logged: true, //this property is not used, as loggin has its own context-reducer, but I will explore the possibility to centralize it here or not
 };
