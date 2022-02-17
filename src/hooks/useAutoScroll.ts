937afbc65ef15ed939adef8fc07f3a6ec43aeaf4
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-02-17 09:41:53 +0100

    Usage info of autoScroll hook and rename of target id

--- src/hooks/useAutoScroll.ts
+++ src/hooks/useAutoScroll.ts
@@ -1,11 +1,26 @@
 import { useLayoutEffect } from "react";
 
-export const useAutoScroll = () => {
-  const scrollTarget = "Past-0";
+/* usage: take the returned value of the hook and pass it as an id to the component you want to auto scroll
+
+const targetToScroll = useAutoScroll( )
+
+...
+return (
+	<div>
+		<div>
+			<div id={targetToScroll} />
+		</div>
+	</div>
+
+)
+
+*/
+export const useAutoScroll = (delay_ms = 500) => {
+  const scrollTarget = "AutoScrollTarget_OnlyOneUniqueId";
   useLayoutEffect(() => {
     setTimeout(() => {
       handleScroll(scrollTarget);
-    }, 500);
+    }, delay_ms);
     setTimeout(() => {
       handleScroll(scrollTarget);
     }, 1000);
