971788ae903af362fd37ea37278f23759988287d
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-02-28 10:34:44 +0100

    Remove display controller by clicking on Topnav

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,21 +1,9 @@
 import * as tw_Layouts from "@/layouts/tw";
 import { Topnav } from "@/components/Topnav/main";
-import React from "react";
-import {
-  useLocalUserPreferences,
-  useLocalUserPreferencesContext,
-} from "@/hooks/useLocalUserPreferences";
-
-interface isetstate {
-  setToogle: React.Dispatch<{ type: string }>;
-}
 
 export const LayoutHeader = () => {
-  const { dispatchLocalState } = useLocalUserPreferencesContext();
   return (
-    <tw_Layouts.TWheader
-      onClick={() => dispatchLocalState({ type: "toggleController" })}
-    >
+    <tw_Layouts.TWheader>
       <Topnav />
     </tw_Layouts.TWheader>
   );
