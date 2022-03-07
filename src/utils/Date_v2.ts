2a854f1c442472fd5e7a6b894148069c06932b6f
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-03-07 15:23:55 +0100

    Date v2: is same date

---
+++ src/utils/Date_v2.ts
@@ -0,0 +1,135 @@
+import { zeroPadd } from "./zeroPadd";
+
+type year =
+  | "2020"
+  | "2021"
+  | "2022"
+  | "2023"
+  | "2024"
+  | "2025"
+  | "2026"
+  | "2027"
+  | "2028"
+  | "2029"
+  | "2030"
+  | "2031"
+  | "2032"
+  | "2033"
+  | "2034"
+  | "2035"
+  | "2036"
+  | "2037"
+  | "2038"
+  | "2039"
+  | "2040";
+
+type month =
+  | "01"
+  | "02"
+  | "03"
+  | "04"
+  | "05"
+  | "06"
+  | "07"
+  | "08"
+  | "09"
+  | "10"
+  | "11"
+  | "12";
+
+type day =
+  | "01"
+  | "02"
+  | "03"
+  | "04"
+  | "05"
+  | "06"
+  | "07"
+  | "08"
+  | "09"
+  | "10"
+  | "11"
+  | "12"
+  | "13"
+  | "14"
+  | "15"
+  | "16"
+  | "17"
+  | "18"
+  | "19"
+  | "20"
+  | "21"
+  | "22"
+  | "23"
+  | "24"
+  | "25"
+  | "26"
+  | "27"
+  | "28"
+  | "29"
+  | "30"
+  | "31";
+
+type ProcessDate = [month, day, year];
+type FormattedDate = `${year}-${month}-${day}`;
+
+// function _composeDate
+function _composeDate(date: ProcessDate): FormattedDate {
+  if (date.length !== 3) {
+    throw Error("composeDate recieved wrong input date");
+  }
+  return `${date[2]}-${date[0]}-${date[1]}`;
+}
+
+function _rangeChecker(
+  callerName: string,
+  value: number,
+  minValueAccepted: number,
+  maxValueAccepted: number
+) {
+  if (minValueAccepted > value || value > maxValueAccepted) {
+    const message = `${callerName} expects a number value in the range: [${minValueAccepted} - ${maxValueAccepted}]`;
+    throw Error(message);
+  }
+}
+
+function _processDate(dt: Date): ProcessDate {
+  return dt
+    .toLocaleString()
+    .split(",") //cleans hour
+    .at(0)! //recover m/d/y
+    .split("/")
+    .map((value) => zeroPadd(parseInt(value))) as ProcessDate;
+}
+function _getTodayDay() {
+  const today = new Date();
+  const p = _processDate(today);
+  return parseInt(p.at(1)!);
+}
+
+export function _renderToday() {
+  const today = new Date();
+  const p = _processDate(today);
+  return _composeDate(p);
+}
+export function _renderDate(
+  year: number,
+  month: number,
+  day: number = _getTodayDay()
+) {
+  _rangeChecker("renderDate", year, 2020, 2040);
+  _rangeChecker("renderDate", month, 1, 12);
+  _rangeChecker("renderDate", day, 1, 31);
+
+  const monthIndex = month - 1; // Date class expects an index for month input
+  const today = new Date(year, monthIndex, day);
+  const p = _processDate(today);
+  return _composeDate(p);
+}
+export const isToday = (
+  year: number,
+  month: number,
+  day: number = _getTodayDay()
+) => {
+  return _renderDate(year, month, day) === _renderToday();
+};
