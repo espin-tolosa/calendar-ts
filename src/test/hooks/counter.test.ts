0966ff994c07ffcfbbfdf4e3765f9fc411c64b43
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-04-27 13:03:51 +0200

    Setup: React Testing Library

---
+++ src/test/hooks/counter.test.ts
@@ -0,0 +1,21 @@
+/** @jest-environment jsdom */
+import { useCounter } from "@/hooks/templates/counter";
+import { renderHook, act } from "@testing-library/react-hooks";
+
+describe("Testing counter hook with React18 with React17 render method", () => {
+  //Disable  console.error for this describe scope: source https://stackoverflow.com/a/58717352/11231828
+  //!Warning: ReactDOM.render is no longer supported in React 18. Use createRoot instead. Until you switch to the new API, your app will behave as if it's running React 17. Learn more: https://reactjs.org/link/switch-to-createroot
+  beforeEach(() => {
+    jest.spyOn(console, "error").mockImplementation(() => {});
+  });
+
+  test("should increment counter", () => {
+    const { result } = renderHook(() => useCounter());
+
+    act(() => {
+      result.current.increment();
+    });
+
+    expect(result.current.count).toBe(1);
+  });
+});
