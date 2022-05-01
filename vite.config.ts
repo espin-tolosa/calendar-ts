235a7ded1737f5598beceaef30bc4b0bd6574c1f
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-05-01 13:55:54 +0200

    Fixing: VSCode slow runtime part-2

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@context": path.resolve(__dirname, "./src/context"),
-      "@interfaces": path.resolve(__dirname, "./src/interfaces"),
     },
   },
 });
