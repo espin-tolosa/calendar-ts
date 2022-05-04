bd308f748513160dbdc1fcfca4a0aee9b91a3a6a
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-05-04 12:31:39 +0200

    Fix: textArea of event edit in mobile and place cursor at the end of the text

--- src/components/Event/textArea.tsx
+++ src/components/Event/textArea.tsx
@@ -2,7 +2,7 @@ import * as StyledEvent from "./tw";
 import { event } from "@/interfaces/index";
 
 import { fetchEvent } from "@/utils/fetchEvent";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export type TextArea = {
   event: event;
@@ -10,13 +10,26 @@ export type TextArea = {
 
 export const EventTextArea = ({ event }: TextArea) => {
   const [state, setState] = useState(event.job);
+  const textRef = useRef<HTMLSpanElement>(null);
 
   return (
     <StyledEvent.TWjobContent>
       <span
+        ref={textRef}
         className="textarea rounded-[5px] w-full p-1 caret-black"
         role="textbox"
         contentEditable={true}
+        onClick={(e) => {
+          console.log("CLICK");
+          e.currentTarget.focus();
+          if (textRef.current) {
+            const range = window.document.createRange();
+            range.selectNodeContents(textRef.current);
+            range.collapse(false);
+            window.getSelection()?.removeAllRanges();
+            window.getSelection()?.addRange(range);
+          }
+        }}
         onKeyDown={(e) => {
           if (e.code === "Enter") {
             const job = e.currentTarget.textContent || "";
@@ -37,3 +50,34 @@ export const EventTextArea = ({ event }: TextArea) => {
     </StyledEvent.TWjobContent>
   );
 };
+
+/*
+onClick={(e) => {
+  console.log("CLICK");
+  e.currentTarget.focus();
+  //console.log("Click", e.currentTarget);
+  eventDispatcher({
+    type: "update",
+    payload: [{ ...event, job: "" }],
+    callback: pushDaysDispatcher,
+  });
+}}
+onKeyDown={(e) => {
+  console.log("KEYDOWN");
+  //console.log(e.code);
+  if (e.code === "Enter") {
+    const job = e.currentTarget.textContent || "";
+    console.log("Dispatching", job);
+    fetchEvent("PUT", { ...event, job });
+    eventDispatcher({
+      type: "update",
+      payload: [{ ...event, job }],
+      callback: pushDaysDispatcher,
+    });
+    e.preventDefault();
+    e.stopPropagation();
+    e.currentTarget.blur();
+  }
+}}
+
+*/
