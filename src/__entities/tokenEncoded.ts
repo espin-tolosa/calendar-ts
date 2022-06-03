2118dc4de0249d9a3e88f537bb743ba226464c43
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-06-04 01:45:35 +0200

    Draft of final token classes hierarchy
    Token class is composed of encodedToken.
    
    This is a proposal that will be tested and ulterly interchanged by
    original class Token and its dependencies.

--- src/__entities/tokenEncoded.ts
+++ src/__entities/tokenEncoded.ts
@@ -1,5 +1,6 @@
 import { encodedTokenFromAPI, token } from "@/interfaces";
-import { safeDecodeJWT_parallel } from "@/modules/jwt";
+import { safeDecodeJWT } from "@/modules/jwt";
+import { DateService } from "@/utils/Date";
 namespace Document {
   export namespace Cookie {
     export function readBodies(): string[] {
@@ -10,57 +11,37 @@ namespace Document {
   }
 }
 
-const hDecodeURI = (cookie: string): encodedTokenFromAPI | undefined => {
-  if (!cookie) {
-    return undefined;
-  }
-
-  let decoded: string;
+const hDecodeURI = (cookie: string): string => {
   try {
-    decoded = window.decodeURIComponent(cookie);
-  } catch {
-    return undefined;
-  }
-
-  if (decoded === "") {
-    return undefined;
-  }
-
-  try {
+    const decoded = window.decodeURIComponent(cookie);
     const encodedToken = JSON.parse(decoded) as encodedTokenFromAPI;
     // This line parses the outcoming cookie to check if it fits the current implementation of the API which sends an object as: {data: "..."}
     // if the parsed cookie doesn't fit this object an nullEncodedToken is returned
+
+    //TODO ref:restore-this-check-encoded
+    //During the refactor, this check wasn't possible to do so its disabled.
     //!if (!checkObjectValidKeys(nameAndType(nullEncodedToken), encodedToken)) {
     //!return undefined;
     //!}
 
-    return encodedToken;
+    return encodedToken.data;
   } catch (e) {
-    return undefined;
+    return "";
   }
 };
 
-function parseURITokens(encodedTokens: string[]) {
-  return encodedTokens.map(hDecodeURI).filter((encodedToken) => !encodedToken);
+function parseURITokens(encodedTokens: string[]): Array<string> {
+  return encodedTokens
+    .map(hDecodeURI)
+    .filter((encodedToken) => !encodedToken.length);
 }
 
-export class TokenEncoded {
-  private readonly state?: token;
+export class TokensDocument {
+  private readonly encodedTokens: Array<string>;
 
   constructor() {
     const cookies = Document.Cookie.readBodies();
-    const result = parseURITokens(cookies);
-
-    //TODO (002): this is proposed to be part of final Token class
-    const tokens = result.map(safeDecodeJWT_parallel).sort((prev, next) => {
-      return next.exp - prev.exp;
-    });
-
-    if (tokens.every((token) => token !== undefined) && tokens.length === 0) {
-      this.state = undefined;
-    } else {
-      this.state = tokens[0] as token;
-    }
+    this.encodedTokens = parseURITokens(cookies);
   }
 
   /**
@@ -68,6 +49,69 @@ export class TokenEncoded {
    * if the data will ended creating valid data in later encoded steps.
    */
   public isEmpty(): boolean {
-    return this.state === undefined;
+    return Boolean(this.encodedTokens.length);
+  }
+
+  public decodeLast(): token {
+    const tokens = this.encodedTokens
+      .map((token) => safeDecodeJWT({ data: token }))
+      .sort((prev, next) => {
+        return next.exp - prev.exp;
+      });
+
+    if (tokens.length === 0) {
+      return Object.freeze({
+        exp: 0,
+        aud: "",
+        data: { iss: "", uid: "", usr: "", aut: "", rus: "" },
+      });
+    } else {
+      return tokens[0] as token;
+    }
+  }
+}
+
+export class LastToken {
+  private readonly source: TokensDocument;
+  private readonly token: token;
+
+  constructor() {
+    this.source = new TokensDocument();
+    this.token = this.source.decodeLast();
+  }
+
+  //TODO ref:class-token-change-data-method
+  //proposed to change this method name to copy()
+  public data() {
+    return Object.freeze(this.token.data);
+  }
+
+  //TODO ref:class-token-change-user-method
+  //proposed to change this method name by showUser()
+  public user() {
+    return this.token.data.usr || "invited";
+  }
+  /**
+   * A valid token is not expired and has a non-empty user name
+   */
+  public isValid(): boolean {
+    const expired = this.token.exp > DateService.secondsSinceEpoch();
+    return expired && !!this.token.data.usr.length;
+  }
+
+  /**
+   * Check authenticated level of the user
+   */
+  public isAuth(level: "read-write" | "read"): boolean {
+    return this.isValid() && this.token.data.aut === level;
+  }
+
+  /**
+   * Check if a friend token has the same id of the current one
+   * friend name is choosen to hint that private fields are accessible
+   * whithin the same class.
+   */
+  public isSameUser(friend: LastToken): boolean {
+    return this.token.data.uid === friend.token.data.uid;
   }
 }
