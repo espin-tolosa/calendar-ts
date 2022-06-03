36f2449a0bdeaba4209c8cdd829de48881870661
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-06-03 22:20:16 +0200

    Final draft of class EncodedToken

--- src/__entities/tokenEncoded.ts
+++ src/__entities/tokenEncoded.ts
@@ -1,13 +1,73 @@
-export class TokenEncoded {
-  private readonly state?: string;
-
-  constructor(data?: string) {
-    this.state = data;
-  }
-
-  public isValid(): boolean {
-    return this.state !== undefined;
+import { encodedTokenFromAPI, token } from "@/interfaces";
+import { safeDecodeJWT_parallel } from "@/modules/jwt";
+namespace Document {
+  export namespace Cookie {
+    export function readBodies(): string[] {
+      return window.document.cookie
+        .split(";")
+        .map((cookie) => cookie.trim().split("=")[1]);
+    }
   }
 }
 
-new TokenEncoded(undefined);
+const hDecodeURI = (cookie: string): encodedTokenFromAPI | undefined => {
+  if (!cookie) {
+    return undefined;
+  }
+
+  let decoded: string;
+  try {
+    decoded = window.decodeURIComponent(cookie);
+  } catch {
+    return undefined;
+  }
+
+  if (decoded === "") {
+    return undefined;
+  }
+
+  try {
+    const encodedToken = JSON.parse(decoded) as encodedTokenFromAPI;
+    // This line parses the outcoming cookie to check if it fits the current implementation of the API which sends an object as: {data: "..."}
+    // if the parsed cookie doesn't fit this object an nullEncodedToken is returned
+    //!if (!checkObjectValidKeys(nameAndType(nullEncodedToken), encodedToken)) {
+    //!return undefined;
+    //!}
+
+    return encodedToken;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+function parseURITokens(encodedTokens: string[]) {
+  return encodedTokens.map(hDecodeURI).filter((encodedToken) => !encodedToken);
+}
+
+export class TokenEncoded {
+  private readonly state?: token;
+
+  constructor() {
+    const cookies = Document.Cookie.readBodies();
+    const result = parseURITokens(cookies);
+
+    //TODO (002): this is proposed to be part of final Token class
+    const tokens = result.map(safeDecodeJWT_parallel).sort((prev, next) => {
+      return next.exp - prev.exp;
+    });
+
+    if (tokens.every((token) => token !== undefined) && tokens.length === 0) {
+      this.state = undefined;
+    } else {
+      this.state = tokens[0] as token;
+    }
+  }
+
+  /**
+   * Checks whether some data has been rescued from cookies, but it don't see
+   * if the data will ended creating valid data in later encoded steps.
+   */
+  public isEmpty(): boolean {
+    return this.state === undefined;
+  }
+}
--- src/modules/jwt.ts
+++ src/modules/jwt.ts
@@ -35,3 +35,29 @@ export function safeDecodeJWT(encodedToken: encodedTokenFromAPI) {
   //Check-passed and return a valid token
   return { ...token, data: { ...token.data } };
 }
+
+export function safeDecodeJWT_parallel(
+  encodedToken?: encodedTokenFromAPI
+): token {
+  const checkValid = typeof encodedToken == "object" && "data" in encodedToken;
+  if (!checkValid) {
+    return nullToken(); //checked
+  }
+
+  let token: token;
+  try {
+    token = jwt_decode<token>(encodedToken.data);
+  } catch {
+    return nullToken(); //checked
+  }
+  //Check decoded token match all the fields of an empty token
+  const { data, ...header } = nullToken();
+  const validHeader = checkObjectValidKeys(nameAndType(header), token);
+  const validData = checkObjectValidKeys(nameAndType(data), token.data);
+  if (!validHeader || !validData) {
+    return nullToken(); //checked
+  }
+
+  //Check-passed and return a valid token
+  return { ...token, data: { ...token.data } };
+}
