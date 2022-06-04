5e7939b40915bea21bdcd86f4a6afcb802df5b48
Samuel Espin Tolosa <samuel.espintolosa@gmail.com>
2022-06-04 17:58:32 +0200

    Refactor Credentials Test to use new testlib model
      Credentials.test.ts didn't applied yet:
    
      import { Test } from "@/testlib";

--- src/__entities/secure-login/Credentials.test.ts
+++ src/__entities/secure-login/Credentials.test.ts
@@ -1,22 +1,24 @@
 /** @jest-environment jsdom */
 import { Test } from "@/testlib";
 
-const token2025 =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NTA4ODY1NTgsImF1ZCI6IjZlZWU0OTg3MGU1YzY5ODlmNTc3MjEyY2NhZDg3YTcxYmNmYzdiZmYiLCJkYXRhIjp7ImlzcyI6ImxvY2FsaG9zdCIsInVpZCI6IjEiLCJ1c3IiOiJzYW11ZWwiLCJhdXQiOiJyZWFkLXdyaXRlIiwicnVzIjoiYWxsIn19.P_8vMNNDQpfJxf65Ra94J64nZnuU4jtmHH85dbzKItE";
-const expidedPHPSESSID = "PHPSESSID=3fc65ec6c17a9cb6482be8378a6414fb";
-const expidedToken = (token: string) =>
-  `762459e65c7f3357f4009b093d237344=%7B%22data%22%3A%22${token}%22%7D`;
 //! Mock window.document and assign a first value
 Object.defineProperty(window.document, "cookie", {
   writable: true,
-  value: `
-	${expidedPHPSESSID};
-	${expidedToken(token2025)}`,
 });
 
-describe("Testing new Token class", () => {
-  test("Test empty cookies should set token class to undefined", () => {
+describe("Testing Class Credentials", () => {
+  test("Valid token", () => {
+    window.document.cookie = `
+		${Test.cookies.sessid.uri};
+		${Test.cookies.jwt.uri}`;
+
     const test = Test.read_headers.credentials();
     expect(test.isValid()).toBe(true);
+  });
+  test("No cookies in document", () => {
+    window.document.cookie = "";
+
+    const test = Test.read_headers.credentials();
+    expect(test.isValid()).toBe(false);
   });
 });
